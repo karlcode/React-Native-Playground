cb16aa5c9c210819fdb1ac5a48194f25ea0ee97c
karlcode <karl.l1@hotmail.com>
2017-05-24 23:51:46 +1000

    First commit

---
+++ index.android.js
@@ -0,0 +1,56 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+
+export default class AwesomeProject extends Component {
+  render() {
+    let pic = {uri: 'http://core0.staticworld.net/images/article/2017/01/pornhub-100704300-large.jpeg'};
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to Karl's chamber of secrets!
+        </Text>
+        <Image source={pic} style={{width: 400, height:200}}/>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
